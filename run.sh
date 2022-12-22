e30c60cee8ec80be6734e07b172453476a6a3d09
Joshua Krall <joshuakrall@pobox.com>
2022-12-22 02:46:34 -0700

    Fix for configuration options

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/with-contenv bashio
 
-MQTTHOST=$(bashio::services mqtt "host")
-MQTTPORT=$(bashio::services mqtt "port")
-MQTTUSER=$(bashio::services mqtt "username")
-MQTTPASS=$(bashio::services mqtt "password")
-CONTROLMYSPA_USER=$(bashio::config 'controlmyspa_user')
-CONTROLMYSPA_PASS=$(bashio::config 'controlmyspa_pass')
-CONTROLMYSPA_CELSIUS=$(bashio::config 'controlmyspa_celsius')
-HASSTOPIC=$(bashio::config 'hass_topic')
-DEBUG=$(bashio::config 'debug')
+export MQTTHOST=$(bashio::services mqtt "host")
+export MQTTPORT=$(bashio::services mqtt "port")
+export MQTTUSER=$(bashio::services mqtt "username")
+export MQTTPASS=$(bashio::services mqtt "password")
+export CONTROLMYSPA_USER=$(bashio::config 'controlmyspa_user')
+export CONTROLMYSPA_PASS=$(bashio::config 'controlmyspa_pass')
+export CONTROLMYSPA_CELSIUS=$(bashio::config 'controlmyspa_celsius')
+export HASSTOPIC=$(bashio::config 'hass_topic')
+export DEBUG=$(bashio::config 'debug')
 
 node app.js
