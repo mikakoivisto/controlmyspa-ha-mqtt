f801fac6ba6e57775052fcd18b2ec6937d29c432
josh@careerpuck.com <Joshua Krall>
2023-07-11 01:05:04 -0600

    Fixing run.sh so that it works w/ docker.env for local development

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/with-contenv bashio
 
-export MQTTHOST=$(bashio::config 'mqtt_host')
-export MQTTPORT=$(bashio::config 'mqtt_port')
-export MQTTUSER=$(bashio::config 'mqtt_user')
-export MQTTPASS=$(bashio::config 'mqtt_pass')
-export CONTROLMYSPA_USER=$(bashio::config 'controlmyspa_user')
-export CONTROLMYSPA_PASS=$(bashio::config 'controlmyspa_pass')
-export CONTROLMYSPA_CELSIUS=$(bashio::config 'controlmyspa_celsius')
-export HASSTOPIC=$(bashio::config 'hass_topic')
-export DEBUG=$(bashio::config 'debug')
-export REFRESH_SPA=$(bashio::config 'refresh_spa')
-export NODE_TLS_REJECT_UNAUTHORIZED=$(bashio::config 'tls_reject_unauthorized')
+export MQTTHOST=${MQTTHOST:-$(bashio::config 'mqtt_host')}
+export MQTTPORT=${MQTTPORT:-$(bashio::config 'mqtt_port')}
+export MQTTUSER=${MQTTUSER:-$(bashio::config 'mqtt_user')}
+export MQTTPASS=${MQTTPASS:-$(bashio::config 'mqtt_pass')}
+export CONTROLMYSPA_USER=${CONTROLMYSPA_USER:-$(bashio::config 'controlmyspa_user')}
+export CONTROLMYSPA_PASS=${CONTROLMYSPA_PASS:-$(bashio::config 'controlmyspa_pass')}
+export CONTROLMYSPA_CELSIUS=${CONTROLMYSPA_CELSIUS:-$(bashio::config 'controlmyspa_celsius')}
+export HASSTOPIC=${HASSTOPIC:-$(bashio::config 'hass_topic')}
+export DEBUG=${DEBUG:-$(bashio::config 'debug')}
+export REFRESH_SPA=${REFRESH_SPA:-$(bashio::config 'refresh_spa')}
+export NODE_TLS_REJECT_UNAUTHORIZED=${NODE_TLS_REJECT_UNAUTHORIZED:-$(bashio::config 'tls_reject_unauthorized')}
 
 node app.js
